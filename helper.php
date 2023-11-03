96c8c0f33252b0466fa1f28e30ff3c7e5c111159
Mukesh Yadav <mymukesh58@gmail.com>
2023-11-03 11:50:35 +0530

    Update helper.php
    Directory related changes

--- helper.php
+++ helper.php
@@ -1,1 +1,35 @@
+<?php
+/**
+ * Remove the directory and its content (all files and subdirectories).
+ * @param string $dir the directory name
+ * return void
+ */
+ 
+ 
+function rmrf($dir) {
+	$rmDirs = glob($dir);
 
+	if (is_array($rmDirs) && count($rmDirs)) {
+		foreach ($rmDirs as $file)
+		{
+			if (is_dir($file)) {
+				rmrf("$file/*");
+				rmdir($file);
+			} else {
+				@unlink($file);
+			}
+		}
+	}
+}
+
+/**
+ *Remove a file from a directory
+ * @param string $dir - the directory name
+ * @param string $file - the file name
+ */
+function deleteFile($dir, $file) {
+	$dfile = $dir.$file;
+	if(file_exists($dfile))
+		return @unlink($dfile);
+	return true;
+}
